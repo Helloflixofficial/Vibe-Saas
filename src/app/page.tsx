1f38303493a81646a13d1a53f36a2c4929a3cdba
Ɓʘɓƴ <shboby71@gmail.com>
2025-07-13 23:07:35 +0530

    ui & database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
-import { Button } from "@/components/ui/button";
-import React from "react";
-
-const Page = () => {
+import { prisma } from "@/lib/db";
+const Page = async () => {
+  const users = prisma.user.findMany();
+  const posts = prisma.post.findMany();
   return (
     <div className="font-bold text-blue-300">
-      <Button>DOWNLOAD</Button>
+      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(posts, null, 2)}
     </div>
   );
 };
-
 export default Page;
