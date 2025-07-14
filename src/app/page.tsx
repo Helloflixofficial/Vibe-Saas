7072408fe4e336f9866288f95eca35bcc10a5748
Ɓʘɓƴ <shboby71@gmail.com>
2025-07-14 21:41:09 +0530

    05-database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { prisma } from "@/lib/db";
+// import { prisma } from "@/lib/db";
 const Page = async () => {
-  const users = prisma.user.findMany();
-  const posts = prisma.post.findMany();
   return (
     <div className="font-bold text-blue-300">
-      {JSON.stringify(users, null, 2)}
-      {JSON.stringify(posts, null, 2)}
+      <h1>hello sire</h1>
     </div>
   );
 };
