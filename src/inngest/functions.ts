08ca14eb89a3ee36f6c2a92bb88eeca4cef036af
Ɓʘɓƴ <shboby71@gmail.com>
2025-07-22 22:30:46 +0530

    E2B:setup;17

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -12,11 +12,11 @@ export const helloWorldGemini = inngest.createFunction(
         if (!event.data?.value) {
             throw new Error("No value provided in event data");
         }
-        const systemInstruction = "You are an expert summarizer. You summarize in 2 words.";
+        const systemInstruction = "You are an expert Reactjs developer. You write readable and clean code";
         const userText = event.data.value;
         const codeAgent = await step.run("codeAgent", async () => {
             const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-            const prompt = `${systemInstruction}\n\nSummarize the following text: "${userText}"`;
+            const prompt = `${systemInstruction}\n\nwrite the following smippit: "${userText}"`;
             console.log(" Running Gemini with prompt:", prompt);
             const result = await model.generateContent(prompt);
             const response = result.response;
@@ -28,18 +28,3 @@ export const helloWorldGemini = inngest.createFunction(
 );
 
 
-
-
-// import { inngest } from "./client";
-
-// export const helloWorld = inngest.createFunction(
-//     { id: "hello-world" },
-//     { event: "test/hello.world" },
-//     async ({ event }) => {
-//         console.log("=== INNGEST FUNCTION STARTED ===");
-//         console.log("Event data value:", event.data?.value);
-//         const mockOutput = "test codeAgent";
-//         console.log("✅ Mock output:", mockOutput);
-//         return { output: mockOutput };
-//     }
-// );
